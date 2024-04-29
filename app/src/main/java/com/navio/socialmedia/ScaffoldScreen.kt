bab2d0203b2730a6c5e91d087bb5d2093c755873
Jose Navio <jnavio@aplitop.com>
2024-04-29 15:47:49 +0200

    feature: Launch a snack bar on the scaffold

--- app/src/main/java/com/navio/socialmedia/ScaffoldScreen.kt
+++ app/src/main/java/com/navio/socialmedia/ScaffoldScreen.kt
@@ -1,5 +1,6 @@
 package com.navio.socialmedia
 
+import android.util.Log
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.IconButton
@@ -7,20 +8,36 @@ import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.Icon
-import androidx.compose.material3.Scaffold
+import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 
 @Composable
 fun ScaffoldScreen() {
+
+    val scaffoldState = rememberScaffoldState()
+    val coroutineScope = rememberCoroutineScope()
+
     Scaffold(
-        topBar = { ScaffoldTopAppBar() }
+        topBar = {
+            ScaffoldTopAppBar() {
+                coroutineScope.launch {
+                    Log.d("Navio_Snack", "Launching...$it")
+                    scaffoldState.snackbarHostState.showSnackbar("You clicked on $it")
+                }
+            }
+        },
+        //todo Test why it does not work with material3
+        scaffoldState = scaffoldState,
     ) { paddingValues ->
         Box(modifier = Modifier.padding(paddingValues)) {
 
@@ -29,15 +46,31 @@ fun ScaffoldScreen() {
 }
 
 @Composable
-fun ScaffoldTopAppBar() {
+fun ScaffoldTopAppBar(onClickIcon: (String) -> Unit) {
     TopAppBar(
         title = { Text("App Title") },
         backgroundColor = Color.Red,
         contentColor = Color.White, elevation = 4.dp, navigationIcon = {
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { onClickIcon("Back") }) {
                 Icon(
                     painter = painterResource(id = R.drawable.icon_arrow_back),
                     contentDescription = "Back",
+                    tint = Color.White
+                )
+            }
+        },
+        actions = {
+            IconButton(onClick = { onClickIcon("Add") }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_add),
+                    contentDescription = "Add",
+                    tint = Color.White
+                )
+            }
+            IconButton(onClick = { onClickIcon("Delete") }) {
+                Icon(
+                    painter = painterResource(id = R.drawable.icon_delete),
+                    contentDescription = "Delete",
                     tint = Color.White
                 )
             }
